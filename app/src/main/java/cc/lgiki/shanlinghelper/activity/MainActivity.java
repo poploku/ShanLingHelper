a360f23b234c9f448e1195246f08603faa20d8fb
LGiki <qq547221432@gmail.com>
2019-04-05 13:24:44 +0800

    Add an AlertDialog for enter ShanLing WiFi transfer URL

--- app/src/main/java/cc/lgiki/shanlinghelper/activity/MainActivity.java
+++ app/src/main/java/cc/lgiki/shanlinghelper/activity/MainActivity.java
@@ -1,18 +1,29 @@
 package cc.lgiki.shanlinghelper.activity;
 
+import android.Manifest;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.List;
 
 import cc.lgiki.shanlinghelper.R;
+import pub.devrel.easypermissions.EasyPermissions;
+import studio.carbonylgroup.textfieldboxes.ExtendedEditText;
+import studio.carbonylgroup.textfieldboxes.TextFieldBoxes;
+import util.ToastUtil;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements EasyPermissions.RationaleCallbacks, EasyPermissions.PermissionCallbacks {
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
@@ -21,6 +32,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        if (!EasyPermissions.hasPermissions(this, permissions)) {
+            EasyPermissions.requestPermissions(this, this.getResources().getString(R.string.permission_rationale), 1, permissions);
+        }
+        initView();
+        View view = LayoutInflater.from(this).inflate(R.layout.alertdialog_shanling_url_input, null);
+        ExtendedEditText extendedEditText = view.findViewById(R.id.extended_edit_text);
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.hint_enter_shanling_url)
+                .setView(view)
+                .setPositiveButton(R.string.btn_ok, ((dialog, which) -> {
+                    ToastUtil.showLongToast(this, extendedEditText.getText().toString());
+                }))
+                .setNegativeButton(R.string.btn_cancel, ((dialog, which) -> {
+                    ToastUtil.showLongToast(this, R.string.message_no_enter_shanling_url);
+                    finish();
+                }))
+                .show();
+    }
+
+    private void initView() {
         toolbar = (Toolbar) findViewById(R.id.tb_main);
         drawerLayout = (DrawerLayout) findViewById(R.id.dl_main);
         navigationView = (NavigationView) findViewById(R.id.nav_view);
@@ -31,13 +63,17 @@ public class MainActivity extends AppCompatActivity {
             actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
         }
         navigationView.setCheckedItem(R.id.nav_upload);
-        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                drawerLayout.closeDrawers();
-                return true;
-            }
-        });
+        navigationView.setNavigationItemSelectedListener(menuItem -> {
+                    drawerLayout.closeDrawers();
+                    return true;
+                }
+        );
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
     }
 
     @Override
@@ -50,5 +86,27 @@ public class MainActivity extends AppCompatActivity {
                 break;
         }
         return true;
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+        ToastUtil.showLongToast(this, R.string.message_no_permission);
+        finish();
+    }
+
+    @Override
+    public void onRationaleAccepted(int requestCode) {
+
+    }
+
+    @Override
+    public void onRationaleDenied(int requestCode) {
+        ToastUtil.showLongToast(this, R.string.message_no_permission);
+        finish();
     }
 }
---
+++ app/src/main/java/util/ToastUtil.java
@@ -0,0 +1,44 @@
+package util;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastUtil {
+    private static Toast toast;
+
+    public static void showShortToast(Context context, String content) {
+        if (toast == null) {
+            toast = Toast.makeText(context, content, Toast.LENGTH_SHORT);
+        } else {
+            toast.setText(content);
+        }
+        toast.show();
+    }
+
+    public static void showShortToast(Context context, int contentRes) {
+        if (toast == null) {
+            toast = Toast.makeText(context, context.getResources().getString(contentRes), Toast.LENGTH_SHORT);
+        } else {
+            toast.setText(context.getResources().getString(contentRes));
+        }
+        toast.show();
+    }
+
+    public static void showLongToast(Context context, String content) {
+        if (toast == null) {
+            toast = Toast.makeText(context, content, Toast.LENGTH_LONG);
+        } else {
+            toast.setText(content);
+        }
+        toast.show();
+    }
+
+    public static void showLongToast(Context context, int contentRes) {
+        if (toast == null) {
+            toast = Toast.makeText(context, context.getResources().getString(contentRes), Toast.LENGTH_LONG);
+        } else {
+            toast.setText(context.getResources().getString(contentRes));
+        }
+        toast.show();
+    }
+}
