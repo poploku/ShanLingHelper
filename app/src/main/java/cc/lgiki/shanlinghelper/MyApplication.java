0984c4311a529e0cafa3997e2d437e6ba0f9e0be
LGiki <qq547221432@gmail.com>
2019-04-11 15:13:03 +0800

    1. Migrate to AndroidX 2. Add android-upload-service library

--- app/src/main/java/cc/lgiki/shanlinghelper/MyApplication.java
+++ app/src/main/java/cc/lgiki/shanlinghelper/MyApplication.java
@@ -2,8 +2,17 @@ package cc.lgiki.shanlinghelper;
 
 import android.app.Application;
 
+import net.gotev.uploadservice.UploadService;
+
 public class MyApplication extends Application {
     private static String shanLingWiFiTransferBaseUrl;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        UploadService.NAMESPACE = BuildConfig.APPLICATION_ID;
+        UploadService.NAMESPACE = "cc.lgiki.shanlinghelper";
+    }
 
     public static String getShanLingWiFiTransferBaseUrl() {
         return shanLingWiFiTransferBaseUrl;
